d35faa2812e0ec591f524a5b6e50e5933912ec09
Sathesh Rgs <satheshrgs@gmail.com>
2019-04-24 08:25:00 +0530

    Update for Heroku Port Specification

--- form.py
+++ form.py
@@ -33,4 +33,5 @@ def downloadFile():
                      attachment_filename='test.xlsx',
                      as_attachment=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
